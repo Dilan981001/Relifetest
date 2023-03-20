e6605edf14b6138293695e9c67cd7d3b3b4cd74b
yathu1 <yathu2198@gmail.com>
2023-03-20 22:42:07 +0530

    add some responsible to nav bar

--- src/components/Marketplace/NavBar.vue
+++ src/components/Marketplace/NavBar.vue
@@ -1,83 +1,129 @@
 <template>
-  <q-header class="header-container full-width items-center bg-primary text-white row no-wrap">
-    <!-- <q-list >
-        <q-item clickable v-ripple>
-        <q-item-section>Image avatar</q-item-section>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="../assets/logo.png" style="width: 100%; height: 100%;">
-          </q-avatar>
-        </q-item-section>
-      </q-item>
-    </q-list> -->
-    <q-toolbar class="col-2  ">
-      <q-btn class="mobile-only" dense flat round icon="menu" @click="toggleLeftDrawer()" />
-      <q-toolbar-title class="text-weight-bold text-h5 text-left"> Relife </q-toolbar-title>
-    </q-toolbar>
-    <div class="col-6 desktop-only">
-      <ul class="my-list  text-weight-bold">
+ 
+  <q-header
+    class="header-container full-width items-center justify-around bg-primary text-white row q-pt-sm q-pb-sm"
+  >
+    <div class="logo">
+      <q-btn
+        class="btn"
+        dense
+        flat
+        round
+        icon="menu"
+        @click="toggleList()"
+      />
+      <router-link to="/" class="text-weight-bold title text-h5 text-left"
+        >Relife</router-link
+      >
+    </div>
+    
+      <ul v-bind:class="{hide:hide}" class="my-list text-weight-bold">
         <li><router-link to="/">Home</router-link></li>
         <li>
           <router-link to="/about">Childrens Home</router-link>
         </li>
         <li>
-          <a href="https://www.google.com" target="_blank">Elders Home</a>
+          <router-link to="/about">Elders Home</router-link>
         </li>
         <li>
-          <a href="https://www.google.com" target="_blank">Notifications</a>
+          <router-link to="/about">Notifications</router-link>
         </li>
-        <li><a href="https://www.google.com" target="_blank">Donate</a></li>
+        <li><router-link to="/about">Donate</router-link></li>
       </ul>
-    </div>
-    <div class="col-4 desktop-only self-center">
-      <div class="col-2  ">
-        <q-btn color="white" text-color="black" label="Buy Products" size="70%" class="text-weight-bold q-ml-md" />
-        <q-btn color="white" text-color="black" label="Log in / Sign Up" size="70%" class="text-weight-bold q-ml-md" />
+   
+    <div class="btn-main self-center">
+      <div v-bind:class="{hide:hide}">
+        <q-btn
+          color="white"
+          text-color="black"
+          label="Buy Products"
+          size="70%"
+          class="text-weight-bold q-ml-md"
+        />
+        <q-btn
+          color="white"
+          text-color="black"
+          label="Log in / Sign Up"
+          size="70%"
+          class="text-weight-bold q-ml-md"
+        />
       </div>
-      <div class="col-2"></div>
+      <div class=""></div>
     </div>
   </q-header>
 </template>
 
 <script>
-
 export default {
-
+  data() {
+    return {
+      showMenu: false,
+      hide: true
+    };
+  },
   methods:{
-    toggleLeftDrawer(){
-      this.$emit('toggle:LeftDrawer');
+    toggleList(){
+      this.hide=!this.hide;
     }
-  }
-}
+}}
 </script>
 
 <style scoped>
+.header-container {
+  top: 0;
+  left: 0;
+  position: sticky;
+}
+
+.button-container {
+  display: inline-block;
+  padding: 0 100px;
+}
+
+.my-list li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+.my-list a {
+  text-decoration: none;
+  color: white;
+  border-bottom: 2px solid transparent;
+}
+
+.my-list a:hover {
+  border-bottom: 2px solid white;
+}
+.logo .btn {
+  display: none;
+}
+
+@media all and (max-width: 864px) {
   .header-container {
-    padding: 0;
-    top:0;
-    left:0;
-    position: sticky;
+    flex-direction: column;
   }
-
-  .button-container {
-    display: flex;
-    justify-content: space-around;
-    padding: 0 100px;
+  ul, .btn-main {
+    display: block;
   }
-
-  .my-list {
-    list-style: none;
-    display: flex;
-    justify-content: space-between;
+  .my-list li {
+    display: block;
+    padding: 5px 0;
+    text-align: center;
   }
-
-  .my-list a {
-    text-decoration: none;
-    color: white;
-    border-bottom: 2px solid transparent;
+  .logo {
+    align-self: flex-start;
+    margin-left: 20px;
   }
-
-  .my-list a:hover{
-    border-bottom: 2px solid white;
+  .logo .btn {
+    display: inline-block;
+    margin-right: 5px;
   }
+  .hide{
+    display: none;
+  }
+}
 </style>
--- src/components/Marketplace/ProductFilter.vue
+++ src/components/Marketplace/ProductFilter.vue
@@ -1,15 +1,21 @@
 <template>
-     <div>
+     <div class="q-mt-md">
         <div class="row row1 no-wrap">
           <div class="">
             <h5 class="text-white text-center">CHOOSE PRODUCTS</h5>
           </div>
-          <!-- <div  class=" col-3 q-mt-auto">
-          <q-icon name="shopping_cart" color="white"  size="md"/> 
-        </div> -->
         </div>
         <div class="row row1 text-white text-weight-bold">
-          <q-list class="row">
+          <q-list class="row desktop-only">
+            <q-item>
+              <q-item-label>ALL</q-item-label>
+            </q-item>
+            <q-item> <q-item-label>ON SALE</q-item-label></q-item>
+
+            <q-item><q-item-label>TRENDING NOW</q-item-label></q-item>
+            <q-item> <q-item-label>NEW ARRIVAL</q-item-label></q-item>
+          </q-list>
+          <q-list class="row q-mt-sm q-mb-sm no-wrap text-center mobile-only">
             <q-item>
               <q-item-label>ALL</q-item-label>
             </q-item>
@@ -19,7 +25,7 @@
             <q-item> <q-item-label>NEW ARRIVAL</q-item-label></q-item>
           </q-list>
         </div>
-        <div class="row row2">
+        <div class="row desktop-only row2">
           <div class="col greenbox ">
             <div>
               <h6 class="sort-text">SORT BY :</h6>
@@ -33,7 +39,6 @@
                 dense
                 color="black"
                 label-color="black"
-               
               />
             </div>
           </div>
@@ -41,6 +46,28 @@
           <div class="col col-input">
             <SearchBox />
             <div class="extra-div"></div>
+          </div>
+        </div>
+        <div class=" mobile-only ">
+          <div class="row justify-evenly q-mb-md">
+            <div>
+              <h6 class="">SORT BY :</h6>
+            </div>
+            <div class="sort" >
+              <q-select
+                class="text-black bg-white"
+                label="Select"
+                v-model="model"
+                :options="options"
+                dense
+                color="black"
+                label-color="black"
+              />
+            </div>
+          </div>
+
+          <div class="search text-center">
+            <SearchBox />
           </div>
         </div>
       </div>
@@ -107,5 +134,9 @@ export default{
   display: flex;
   justify-content: center;
 }
-
+.search{
+  width:75%;
+  text-align: center;
+  margin: 0 auto;
+}
 </style>
